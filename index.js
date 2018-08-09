2f652a4912a022fb15deac590f890698bd36e8b8
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-09 22:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+function theBeatlesPlay(musician,instrument){
+  var array = []
+  for (let i = 0; i < musician.length; i++){
+    array.push(musician[i] +" plays " + instrument[i])
+  }
+  return array
+}
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  let i = 0
+  var newFactsArray = []
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return newFactsArray
+}
